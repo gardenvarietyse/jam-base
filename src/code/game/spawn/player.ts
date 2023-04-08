a1194bc929dcb4d57fa06a2930896ec3608bbba4
steve <pxxonline@gmail.com>
2023-04-08 22:43:39 +0200

    feat: implement generic character controller

--- src/code/game/spawn/player.ts
+++ src/code/game/spawn/player.ts
@@ -3,6 +3,7 @@ import { ILDTKEntityInstance } from '../../lib/ldtk/format';
 import { GameEntity } from '../system/entity';
 import { Asset } from '../../../asset';
 import { make_body } from '../system/blomp/blomp';
+import { make_controller } from '../system/character/controller';
 
 export const spawnPlayer = (
   world: World<GameEntity>,
@@ -18,6 +19,7 @@ export const spawnPlayer = (
     },
     body: make_body(16, 16),
     character_animator: true,
+    controller: make_controller(),
   });
 
   return entity;
